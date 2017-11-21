ed600d587b9172c56135ddd42035242506344a82
Diego Seminario <diegoasc@gmail.com>
2017-11-20 21:52:51 -0500

    Adding CORS to Lumen

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
   'supportsCredentials' => false,
   'allowedOrigins' => ['*'],
   'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
-  'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+  'allowedMethods' => ['GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
   'exposedHeaders' => [],
   'maxAge' => 0,
 ];
