32971e59fd990b1da1266f121cb7e145c929daff
Andreé Sandoval Rojas <andree.bit@gmail.com>
2017-11-21 20:25:46 -0500

    subiendo archivo de base de datos

---
+++ database/database.sql
@@ -0,0 +1,457 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Versión 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.52)
+# Base de datos: tp3_backend
+# Tiempo de Generación: 2017-11-22 01:11:24 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Volcado de tabla carriers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `carriers`;
+
+CREATE TABLE `carriers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_id` int(10) unsigned NOT NULL,
+  `driver_license` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `carriers_employee_id_foreign` (`employee_id`),
+  CONSTRAINT `carriers_employee_id_foreign` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla employees
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `employees`;
+
+CREATE TABLE `employees` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `user_uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `role_id` int(10) unsigned NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `employees_role_id_foreign` (`role_id`),
+  CONSTRAINT `employees_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla inventory_movement_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `inventory_movement_has_products`;
+
+CREATE TABLE `inventory_movement_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `inventory_movement_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `inventory_movement_has_products_inventory_movement_id_foreign` (`inventory_movement_id`),
+  KEY `inventory_movement_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `inventory_movement_has_products_inventory_movement_id_foreign` FOREIGN KEY (`inventory_movement_id`) REFERENCES `inventory_movements` (`id`),
+  CONSTRAINT `inventory_movement_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla inventory_movements
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `inventory_movements`;
+
+CREATE TABLE `inventory_movements` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `warehouse_id` int(10) unsigned NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `type` enum('in','out') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `inventory_movements_warehouse_id_foreign` (`warehouse_id`),
+  CONSTRAINT `inventory_movements_warehouse_id_foreign` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla migrations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla order_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `order_has_products`;
+
+CREATE TABLE `order_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_has_products_order_id_foreign` (`order_id`),
+  KEY `order_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `order_has_products_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla orders
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `warehouse_id` int(10) unsigned NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `status` enum('pending','completed','canceled') COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `orders_warehouse_id_foreign` (`warehouse_id`),
+  CONSTRAINT `orders_warehouse_id_foreign` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla product_categories
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `product_categories`;
+
+CREATE TABLE `product_categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `product_category_id` int(10) unsigned NOT NULL,
+  `sku` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `unit_of_measurement` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `products_product_category_id_foreign` (`product_category_id`),
+  CONSTRAINT `products_product_category_id_foreign` FOREIGN KEY (`product_category_id`) REFERENCES `product_categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla quotation_request_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `quotation_request_has_products`;
+
+CREATE TABLE `quotation_request_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `quotation_request_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `quotation_request_has_products_quotation_request_id_foreign` (`quotation_request_id`),
+  KEY `quotation_request_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `quotation_request_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `quotation_request_has_products_quotation_request_id_foreign` FOREIGN KEY (`quotation_request_id`) REFERENCES `quotation_requests` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla quotation_requests
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `quotation_requests`;
+
+CREATE TABLE `quotation_requests` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `order_id` int(10) unsigned NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('pending','completed','canceled') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `quotation_requests_order_id_foreign` (`order_id`),
+  CONSTRAINT `quotation_requests_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla supplier_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `supplier_has_products`;
+
+CREATE TABLE `supplier_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `supplier_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `supplier_has_products_supplier_id_foreign` (`supplier_id`),
+  KEY `supplier_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `supplier_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `supplier_has_products_supplier_id_foreign` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla suppliers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `suppliers`;
+
+CREATE TABLE `suppliers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla transfer_guide_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transfer_guide_has_products`;
+
+CREATE TABLE `transfer_guide_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transfer_guide_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transfer_guide_has_products_transfer_guide_id_foreign` (`transfer_guide_id`),
+  KEY `transfer_guide_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `transfer_guide_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `transfer_guide_has_products_transfer_guide_id_foreign` FOREIGN KEY (`transfer_guide_id`) REFERENCES `transfer_guides` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla transfer_guides
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transfer_guides`;
+
+CREATE TABLE `transfer_guides` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `order_id` int(10) unsigned NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transfer_guides_order_id_foreign` (`order_id`),
+  CONSTRAINT `transfer_guides_order_id_foreign` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla trucks
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `trucks`;
+
+CREATE TABLE `trucks` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `license_plate` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `brand` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla warehouse_has_products
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `warehouse_has_products`;
+
+CREATE TABLE `warehouse_has_products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `warehouse_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `stock` int(10) unsigned NOT NULL,
+  `minimum_stock` int(10) unsigned NOT NULL,
+  `maximum_stock` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `warehouse_has_products_warehouse_id_foreign` (`warehouse_id`),
+  KEY `warehouse_has_products_product_id_foreign` (`product_id`),
+  CONSTRAINT `warehouse_has_products_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `warehouse_has_products_warehouse_id_foreign` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla warehouses
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `warehouses`;
+
+CREATE TABLE `warehouses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_phone_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `latitude` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `longitude` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('active','inactive') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Volcado de tabla waybills
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `waybills`;
+
+CREATE TABLE `waybills` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `document_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `carrier_id` int(10) unsigned NOT NULL,
+  `truck_id` int(10) unsigned NOT NULL,
+  `warehouse_from_id` int(10) unsigned NOT NULL,
+  `warehouse_to_id` int(10) unsigned NOT NULL,
+  `date_time` datetime NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci,
+  `delivery_status` enum('pending','progress','completed','canceled') COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('active','canceled') COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `order_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `waybills_carrier_id_foreign` (`carrier_id`),
+  KEY `waybills_truck_id_foreign` (`truck_id`),
+  KEY `waybills_warehouse_from_id_foreign` (`warehouse_from_id`),
+  KEY `waybills_warehouse_to_id_foreign` (`warehouse_to_id`),
+  CONSTRAINT `waybills_carrier_id_foreign` FOREIGN KEY (`carrier_id`) REFERENCES `carriers` (`id`),
+  CONSTRAINT `waybills_truck_id_foreign` FOREIGN KEY (`truck_id`) REFERENCES `trucks` (`id`),
+  CONSTRAINT `waybills_warehouse_from_id_foreign` FOREIGN KEY (`warehouse_from_id`) REFERENCES `warehouses` (`id`),
+  CONSTRAINT `waybills_warehouse_to_id_foreign` FOREIGN KEY (`warehouse_to_id`) REFERENCES `warehouses` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
