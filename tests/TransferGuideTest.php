5e2d18209bb8ac11dc49dd9a186e1cb3dd08c999
Andreé Sandoval Rojas <andree.bit@gmail.com>
2017-11-28 20:49:55 -0500

    agregando archivos de prueba

---
+++ tests/TransferGuideTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class TransferGuideTest extends TestCase
+{
+
+    /**
+     *
+     */
+    public function testGet() {
+
+    }
+
+    /**
+     *
+     */
+    public function testGetOne() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPost() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPut() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPatch() {
+
+    }
+
+
+
+}
---
+++ tests/WaybillTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class WaybillTest extends TestCase
+{
+
+    /**
+     *
+     */
+    public function testGet() {
+
+    }
+
+    /**
+     *
+     */
+    public function testGetOne() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPost() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPut() {
+
+    }
+
+    /**
+     *
+     */
+    public function testPatch() {
+
+    }
+
+
+
+}
