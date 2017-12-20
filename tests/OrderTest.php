efbbef622fe87b40229fef2edca3bc19a418a2b8
Andreé Sandoval Rojas <andree.bit@gmail.com>
2017-12-19 19:26:12 -0500

    probando integracion continua con error

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -16,6 +16,7 @@ class OrderTest extends TestCase
         $number3 = $this->invokeMethod($instance, 'documentNumberGenerator', [33]);
         $number4 = $this->invokeMethod($instance, 'documentNumberGenerator', [1000]);
         $number5 = $this->invokeMethod($instance, 'documentNumberGenerator', [999999]);
+        $number6 = $this->invokeMethod($instance, 'documentNumberGenerator', [8]);
 
 
         $this->assertEquals('NP17000031', $number1);
@@ -23,6 +24,7 @@ class OrderTest extends TestCase
         $this->assertEquals('NP17000033', $number3);
         $this->assertEquals('NP17001000', $number4);
         $this->assertEquals('NP17999999', $number5);
+        $this->assertEquals('NP17000009', $number6);
     }
 
 }
